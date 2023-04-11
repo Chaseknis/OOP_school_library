4d10dcd4d70662f899a0f29c673f3063facb42a9
chaseknis <chase6th@gmail.com>
2023-04-12 01:33:45 +0200

    Add nameable.rb file Add capitalize_decorator.rb file Add person_example.rb file to check the code output trimmer_decorator.rb file Add decorator file Run rubocop for errors Update the README.md file

---
+++ capitalize_decorator.rb
@@ -0,0 +1,6 @@
+require './Decorator'
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
---
+++ decorator.rb
@@ -0,0 +1,11 @@
+require './Nameable'
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
